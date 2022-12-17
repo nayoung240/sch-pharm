79badb203bd4ffe27e2c553476439da1924d1788
zerome <iirony@naver.com>
2022-12-17 19:44:05 +0900

    Initial commit

---
+++ src/main/java/com/example/schpharm/SchPharmApplication.java
@@ -0,0 +1,13 @@
+package com.example.schpharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchPharmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchPharmApplication.class, args);
+    }
+
+}
