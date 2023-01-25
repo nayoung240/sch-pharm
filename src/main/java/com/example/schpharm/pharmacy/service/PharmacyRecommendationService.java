90ac40fba13179b95c4d34e9474a26bf8c157fc7
zerome <iirony@naver.com>
2023-01-25 15:27:42 +0900

    추천 결과 저장 기능 구현

---
+++ src/main/java/com/example/schpharm/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,45 @@
+package com.example.schpharm.pharmacy.service;
+
+import com.example.schpharm.api.dto.DocumentDto;
+import com.example.schpharm.api.dto.KakaoApiResponseDto;
+import com.example.schpharm.api.service.KakaoAddressSearchService;
+import com.example.schpharm.direction.entity.Direction;
+import com.example.schpharm.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address) {
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        // retry 모두 실패 또는 빈 결과값인 경우
+        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[PharmacyRecommendationService.recommendPharmacyList fail] Input address: {}", address);
+            return Collections.emptyList();
+        }
+
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
+
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+        //List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
+}
