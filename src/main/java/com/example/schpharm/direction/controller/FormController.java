7aa1aca64a7c763ac04e9cc4e03358a5df1c2067
zerome <iirony@naver.com>
2023-01-26 01:23:33 +0900

    handlebars 추가 적용

---
+++ src/main/java/com/example/schpharm/direction/controller/FormController.java
@@ -0,0 +1,14 @@
+package com.example.schpharm.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
