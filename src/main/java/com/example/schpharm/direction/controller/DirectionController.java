1c40798120b874b50e61ded1d47cdeb7eca73e30
zerome <iirony@naver.com>
2023-03-07 00:25:48 +0900

    shorten url 개발

---
+++ src/main/java/com/example/schpharm/direction/controller/DirectionController.java
@@ -0,0 +1,33 @@
+package com.example.schpharm.direction.controller;
+
+import com.example.schpharm.direction.entity.Direction;
+import com.example.schpharm.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class DirectionController {
+    private final DirectionService directionService;
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
+    // http://{ip}/dir/{encodedId}
+    @GetMapping("/dir/{encodedId}")
+    public String searchDirection(@PathVariable("encodedId") String encodedId) {
+        Direction resultDirection = directionService.findById(encodedId);
+
+        String params = String.join(",", resultDirection.getTargetPharmacyName(),
+                String.valueOf(resultDirection.getTargetLatitude()), String.valueOf(resultDirection.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString(); // 인코딩
+
+        log.info("direction params: {}, url: {}", params, result);
+
+        return "redirect:"+result;
+    }
+}
--- src/main/java/com/example/schpharm/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/schpharm/pharmacy/service/PharmacyRecommendationService.java
@@ -5,9 +5,11 @@ import com.example.schpharm.api.dto.KakaoApiResponseDto;
 import com.example.schpharm.api.service.KakaoAddressSearchService;
 import com.example.schpharm.direction.dto.OutputDto;
 import com.example.schpharm.direction.entity.Direction;
+import com.example.schpharm.direction.service.Base62Service;
 import com.example.schpharm.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,10 +26,15 @@ public class PharmacyRecommendationService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+    private final Base62Service base62Service;
 
     private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
     private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
+    @Value("${pharmacy.recommendation.base.url")
+    private String baseUrl;
+
+    // 추천 프로세스
     public List<OutputDto> recommendPharmacyList(String address) {
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
@@ -40,10 +47,10 @@ public class PharmacyRecommendationService {
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
         // 공공기관 약국 데이터 및 거리계산 알고리즘 이용
-//        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
         // kakao 카테고리를 이용한 장소 검색 api 이용용
-       List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+//       List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
         return directionService.saveAll(directionList)
                 .stream()
@@ -53,18 +60,11 @@ public class PharmacyRecommendationService {
 
     private OutputDto convertToOutputDto(Direction direction) {
 
-        String params = String.join(",", direction.getTargetPharmacyName(),
-                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
-
-        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString(); // 인코딩
-
-        log.info("direction params: {}, url: {}", params, result);
-
         return OutputDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
                 // 길안내
-                .directionUrl(result)
+                .directionUrl(baseUrl + base62Service.encodeDirectionId(direction.getId()))
                 // 로드뷰
                 .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
